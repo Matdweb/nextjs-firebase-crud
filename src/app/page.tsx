7e9ddcc40b89e705cf02f2b04a937fe7b2f0c085
mat_dweb <mat.dweb28@gmail.com>
2025-02-10 19:57:53 -0600

    feat: display tasks with TableTasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { Task } from "@/types/types";
+import TableTasks from "@/components/TableTasks";
 
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -30,30 +31,17 @@ export default function Home() {
 
   return (
     <>
-      <div className="container mx-auto mt-8 max-w-[560px]">
+      <div className="container mx-auto max-w-[560px]">
         <div className="flex justify-between items-center pb-4 border-b border-dashed border-gray-900 mb-4">
-          <h1 className="text-3xl font-semibold">Tasks</h1>
+          <h1 className="text-3xl font-semibold mt-8">Tasks</h1>
           <Link
-            className="bg-green-600 hover:bg-opacity-80 text-white rounded-lg px-4 py-2 duration-200"
+            className="bg-green-600 hover:bg-opacity-80 text-white rounded-lg px-4 py-2 duration-200 mt-8"
             href="/create"
           >
             Create New
           </Link>
         </div>
-        <ul>
-          {tasks.map((task) => (
-            <li key={task.id} className="py-2 flex justify-between w-full">
-              <span>
-                <strong>{task.name}</strong> - {task.description}
-              </span>
-              <span className="flex gap-2">
-                <Link className="text-blue-700 underline hover:no-underline" href={`/edit/${task.id}`}>Edit</Link>
-                <Link className="text-red-500 underline hover:no-underline" href={`/delete/${task.id}`}>Delete</Link>
-              </span>
-            </li>
-          ))}
-          {tasks?.length < 1 && <div className="py-2">No data</div>}
-        </ul>
+        <TableTasks tasks={tasks} />
       </div>
       <Head>
         <title>Task</title>
