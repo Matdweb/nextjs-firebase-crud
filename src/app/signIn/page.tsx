ef04d8da06f97a37b05bbd33f3ec279915d2bb8d
mat_dweb <mat.dweb28@gmail.com>
2025-03-22 00:29:42 -0600

    feat: create singIn page

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,68 +1,109 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { z } from "zod";
 import { Form } from "@heroui/form";
 import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
-import { ValidationError } from "next/dist/compiled/amphtml-validator";
+import { createUser } from "../lib/actions";
+import { useActionState } from "react";
+import { Alert } from "@heroui/alert";
+import { authenticateActionErrors } from "../lib/definitions";
+import { startTransition } from "react";
+import {Spinner} from "@heroui/spinner";
 
 export default function App() {
-    const [password, setPassword] = useState<string>("");
-    const [errors, setErrors] = useState<ValidationError>({});
+    //const [password, setPassword] = useState<string>("");
+    const [serverResponse, formAction, isPending] = useActionState(
+        createUser,
+        undefined,
+    );
+    const [errors, setErrors] = useState<Record<string, string>>({});
 
-    // Real-time password validation
-    const getPasswordError = (value: string) => {
-        const valueLenght = value.length
-        if (valueLenght === 0) return null;
+    const passwordSchema = z.string({
+        required_error: "Password is required",
+    })
+        .min(6, { message: "Password must be 6 characters or more" })
+        .refine((value) => (
+            (value.match(/[A-Z]/g) || []).length < 1 ? false : true
+        ), {
+            message: "Password needs at least 1 uppercase letter"
+        })
+        .refine((value) => (
+            (value.match(/[^a-z]/gi) || []).length < 1 ? false : true
+        ), {
+            message: "Password needs at least 1 symbol"
+        });
 
-        if (valueLenght < 4) {
-            return "Password must be 4 characters or more";
-        }
-        if ((value.match(/[A-Z]/g) || []).length < 1) {
-            return "Password needs at least 1 uppercase letter";
-        }
-        if ((value.match(/[^a-z]/gi) || []).length < 1) {
-            return "Password needs at least 1 symbol";
-        }
+    const validatePassword = (value: string) => {
 
-        return null;
-    };
-
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const data = Object.fromEntries(new FormData(e.currentTarget));
-
-        // Custom validation checks
-        const newErrors: ValidationError = {};
-
-        // Password validation
-        const passwordError = getPasswordError(data.password as string);
-
-        if (passwordError) {
-            newErrors.password = passwordError;
-        }
-
-        // Username validation
-        if (data.name === "admin") {
-            newErrors.name = "Nice try! Choose a different username";
-        }
-
-        if (Object.keys(newErrors).length > 0) {
-            setErrors(newErrors);
-
+        const { password, ...rest } = errors;
+        if (value.length === 0 && password) {
+            setErrors({
+                ...rest
+            });
             return;
         }
 
-        setErrors({});
-    };
+        const result = passwordSchema.safeParse(value);
+
+        if (!result.success) {
+            setErrors({
+                ...errors,
+                password: result.error?.issues[0].message
+            });
+            return;
+        } else {
+            setErrors({
+                ...rest
+            });
+        }
+
+        return null;
+    }
+
+    const formatErrors = () => {
+        const errors: authenticateActionErrors = serverResponse?.errors || {};
+        const formattedErrors = {};
+
+        for (const [key, value] of Object.entries(errors)) {
+            Object.assign(formattedErrors, { [key]: (value[0] || "") })
+        }
+
+        return formattedErrors;
+    }
+
+    const handleServerErrors = () => {
+        if (!(serverResponse?.success)) {
+            setErrors(formatErrors());
+        } else {
+            setErrors({});
+        }
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        startTransition(() => {
+            formAction((new FormData(event.target as HTMLFormElement)))
+        });
+    }
+
+    //TODO: redirect user to signIn when user is created
+    useEffect(() => {
+        handleServerErrors();
+    }, [serverResponse]);
 
     return (
         <section className="w-full">
-            <h1 className="text-2xl font-bold text-center left-0 py-8">Login</h1>
+            <h1 className="text-2xl font-bold text-center left-0 py-8">Sign In</h1>
             <Form
                 className="w-full justify-center items-center space-y-4"
                 validationBehavior="native"
-                validationErrors={errors}
-                onSubmit={(e) => onSubmit(e)}
+                validationErrors={errors}  //This way the error message an invalid fields will be handled automaticly
+                action={formAction}
+                onSubmit={handleSubmit}
+                onReset={() => {
+                    setErrors({});
+                }}
             >
                 <div className="w-full px-8 flex flex-col gap-4 max-w-md">
                     <Input
@@ -71,7 +112,6 @@ export default function App() {
                             if (validationDetails.valueMissing) {
                                 return "Please enter your name";
                             }
-
                             return errors.name;
                         }}
                         label="Name"
@@ -89,6 +129,8 @@ export default function App() {
                             if (validationDetails.typeMismatch) {
                                 return "Please enter a valid email address";
                             }
+
+                            return errors.email;
                         }}
                         label="Email"
                         labelPlacement="outside"
@@ -99,24 +141,64 @@ export default function App() {
 
                     <Input
                         isRequired
-                        errorMessage={getPasswordError(password)}
-                        isInvalid={getPasswordError(password) !== null}
+                        errorMessage={({ validationDetails }) => {
+                            if (validationDetails.valueMissing) {
+                                return "Please enter your password";
+                            }
+
+                            return errors.password;
+                        }}
                         label="Password"
                         labelPlacement="outside"
                         name="password"
                         placeholder="Enter your password"
                         type="password"
-                        value={password}
-                        onValueChange={setPassword}
+                        onValueChange={validatePassword}
                     />
                     <div className="flex gap-4">
-                        <Button className="w-full" color="primary" type="submit">
-                            Submit
+                        <Button
+                            className="w-full flex justify-center items-center"
+                            color="primary"
+                            type="submit"
+                            aria-disabled={isPending}
+                            isDisabled={isPending}
+                        >
+                            {
+                                isPending ?
+                                    <Spinner color="white" variant="dots" />
+                                    :
+                                    "Sign In"
+                            }
                         </Button>
-                        <Button type="reset" variant="bordered">
+                        <Button
+                            type="reset"
+                            variant="bordered"
+                        >
                             Reset
                         </Button>
                     </div>
+
+                    {
+                        serverResponse && (
+                            (serverResponse?.success) ?
+                                <div className="w-full flex items-center my-3">
+                                    <Alert
+                                        color={"success"}
+                                        title={serverResponse.message[0]}
+                                        description={serverResponse.message[1] || ""}
+                                    />
+                                </div>
+                                :
+                                <div className="w-full flex items-center my-3">
+                                    <Alert
+                                        color={"danger"}
+                                        title={serverResponse?.message}
+                                        description={serverResponse.message[1] || ""}
+
+                                    />
+                                </div>
+                        )
+                    }
                 </div>
             </Form>
         </section>
