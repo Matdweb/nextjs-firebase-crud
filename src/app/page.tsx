ea6baccd1aea7d2439d99290d66ed6469618e40b
mat_dweb <mat.dweb28@gmail.com>
2025-03-31 18:12:59 -0600

    fix: remove unnecessary importrs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,9 @@ import { Task } from "@/types/types";
 import TableTasks from "@/components/TableTasks";
 import { Button } from "@heroui/button";
 import { redirect } from 'next/navigation'
-import { useSession } from "@/context/SessionContext";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const { session } = useSession()
-  const router = useRouter();
 
   const getTasks = async () => {
     try {
